dcf8aec802df0b08dc9b44afa36959d0adcd61a1
Jesse Isokääntä <83401915+Archilon@users.noreply.github.com>
2022-03-30 10:50:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Phonebook from 'components/Phonebook_app/Phonebook.js'
-import Feedback from 'components/Feedback_app/Feedback.js'
+import Phonebook from './src/components/Phonebook_app/Phonebook.js'
+import Feedback from './src/components/Feedback_app/Feedback.js'
 
-import Reminder from 'components/Reminder_app/Reminder.js'
+import Reminder from './src/components/Reminder_app/Reminder.js'
 const Main = () => {
 
 
