df8457ef3ce70a516730e35caae5eda65d215374
Jesse Isokääntä <83401915+Archilon@users.noreply.github.com>
2022-03-30 10:49:54 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Phonebook from 'components/Phonebook_app/Phonebook.js'
+import Feedback from 'components/Feedback_app/Feedback.js'
+
+import Reminder from 'components/Reminder_app/Reminder.js'
+const Main = () => {
+
+
+  return (
+    <div>
+      <Phonebook />
+      <Feedback />
+      <Reminder />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+)
